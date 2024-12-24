3b510cb60093cb2ebcf352fca192101ab132b8f9
MiroslawBaca <rembit602@gmail.com>
2024-12-24 22:45:49 +0100

    christmas update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main() {
 	//SystemInit_Custom();
-    UART uart(9600);
-    uart.Println("Hello Worlddsadasd!");
-	uart.Println("nibba!");
+    Uart uart(9600);
+    uart.println("Hello Worlddsadasd!");
+		uart.println("Test2!");
 
 }
